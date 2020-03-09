ff989f4e15f3f54c266a6d32a47534cfbca6133c
unknown <jeff.a.cate@gmail.com>
2020-03-09 17:01:33 -0500

    Practice problem 3 complete average of student averages calculated.

--- LINQ_PracticeProblemsApp/LINQ_PracticeProblemsApp/LINQ-PracticeProblems.cs
+++ LINQ_PracticeProblemsApp/LINQ_PracticeProblemsApp/LINQ-PracticeProblems.cs
@@ -16,14 +16,21 @@ namespace LINQ_PracticeProblemsApp
             words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
             var wordsWithTH = words.Where(words => words.Contains("th")); 
             foreach (var word  in wordsWithTH){ Console.WriteLine(word); }
+            Console.WriteLine();
 
             words = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike", "Duplicate", "Duplicate" };
             var wordsWithoutDuplicates = words.Distinct();
             foreach (var word in wordsWithoutDuplicates) { Console.WriteLine(word); }
-
             Console.WriteLine();
-            Console.ReadKey();
 
+            List<string> studentAsStrings = new List<string>(){ "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" };
+            var ArrayOfDouble = studentAsStrings.Select(student => student.Split(',')
+                                                                          .Select(grades => Convert.ToDouble(grades)))
+                                                                          .Select(student => student.Where(grade => grade != student.Min()))
+                                                                          .Select(grades => grades.Average())
+                                                                          .Average();
+            Console.WriteLine(ArrayOfDouble);
+            Console.ReadKey();
         }
 
         
